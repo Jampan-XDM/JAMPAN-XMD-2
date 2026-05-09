b1d07329bac3a574dd5e6e1e42cbadbd8de46e91
Jampan-XDM <jampankelvin@gmail.com>
2026-05-09 17:41:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,23 @@
 export const config = {
-  botName: "JAMPAN-XMD",
+
+  // 👑 OWNER INFO
   ownerName: "Kelvin Jampan",
   ownerNumber: "255674229015",
-  port: process.env.PORT || 3000
+
+  // 📢 CHANNELS
+  channel: "https://whatsapp.com/channel/0029Vb7fTNf3QxS8A6rbBB3S",
+  youtube: "https://youtube.com/@jampani-xmd?si=p2JqL8PRL4ZXppzr",
+
+  // 🚀 SERVER
+  port: process.env.PORT || 3000,
+
+  // 🎨 UI
+  botName: "JAMPAN-XMD",
+  thumbnail: "https://files.catbox.moe/fzjhed.png",
+
+  // ⚡ SYSTEM
+  sessionFolder: "sessions",
+  maxRetry: 3,
+  pairTimeout: 10000,
+
 };
