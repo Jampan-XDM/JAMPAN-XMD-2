c1f05c4c98f3d03c67d58a8465ac173df98212f9
Jampan-XDM <jampankelvin@gmail.com>
2026-05-09 18:03:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,7 @@
-export const config = {
-
-  // 👑 OWNER INFO
-  ownerName: "Kelvin Jampan",
-  ownerNumber: "255674229015",
-
-  // 📢 CHANNELS
-  channel: "https://whatsapp.com/channel/0029Vb7fTNf3QxS8A6rbBB3S",
-  youtube: "https://youtube.com/@jampani-xmd?si=p2JqL8PRL4ZXppzr",
-
-  // 🚀 SERVER
-  port: process.env.PORT || 3000,
-
-  // 🎨 UI
-  botName: "JAMPAN-XMD",
-  thumbnail: "https://files.catbox.moe/fzjhed.png",
-
-  // ⚡ SYSTEM
-  sessionFolder: "sessions",
-  maxRetry: 3,
-  pairTimeout: 10000,
-
+module.exports = {
+    botName: "JAMPAN-XMD",
+    ownerName: "Kelvin Jampan",
+    channelLink: "https://whatsapp.com/channel/0029Vb7fTNf3QxS8A6rbBB3S",
+    channelJID: "120363409292513352@newsletter",
+    pairingVibe: "⚡ JAMPAN-XMD PAIRING SYSTEM"
 };
