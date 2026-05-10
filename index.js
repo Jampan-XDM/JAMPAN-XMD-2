ef420fe914b0dd380d2bbd7e1e03760316a87133
Jampan-XDM <jampankelvin@gmail.com>
2026-05-10 21:37:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const PORT = process.env.PORT || 7860;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '')));
 
-// Inatoa HTML ya pairing moja kwa moja kwenye link kuu
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Endpoint ya kuomba code
 app.get('/code', async (req, res) => {
     const number = req.query.number;
-    if (!number) return res.status(400).json({ error: "No number" });
+    if (!number) return res.status(400).json({ error: "Weka namba ya simu!" });
 
     try {
         const pairingCode = await startPairing(number);
         res.json({ code: pairingCode });
     } catch (err) {
-        res.status(500).json({ error: "Server Error" });
+        console.error(err);
+        res.status(500).json({ error: "Server imeshindwa kutoa kodi. Subiri dakika 15 uone kama utakuwa umekatwa block na WhatsApp." });
     }
 });
 
