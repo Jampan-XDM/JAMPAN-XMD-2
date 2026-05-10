3157e12788a06b3a849dec13976acd0bfa218452
Jampan-XDM <jampankelvin@gmail.com>
2026-05-10 11:22:23 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,31 +1,26 @@
 const { default: makeWASocket, useMultiFileAuthState, Browsers, delay, fetchLatestBaileysVersion } = require("@whiskeysockets/baileys");
 const pino = require("pino");
-const fs = require('fs');
 
 async function startPairing(phoneNumber) {
-    // Kila ombi linatengeneza session ya muda (temp) kuzuia migongano
-    const { state, saveCreds } = await useMultiFileAuthState('auth_info');
+    const { state, saveCreds } = await useMultiFileAuthState('session');
     const { version } = await fetchLatestBaileysVersion();
 
     const sock = makeWASocket({
         version,
         auth: state,
         printQRInTerminal: false,
-        logger: pino({ level: "silent" }), // Muhimu kuzuia RAM overload
-        browser: Browsers.macOS("Chrome"), // Inadanganya WA iwe thabiti
+        logger: pino({ level: "silent" }),
+        browser: Browsers.macOS("Chrome"),
         syncFullHistory: false
     });
 
-    // Subiri kidogo socket iwe imetulia
-    await delay(3000);
+    await delay(3000); // Subiri socket itulie
 
     if (!sock.authState.creds.registered) {
         try {
-            // Omba code
             let code = await sock.requestPairingCode(phoneNumber);
             return code;
         } catch (error) {
-            console.error("Error requesting code:", error);
             throw error;
         }
     }
