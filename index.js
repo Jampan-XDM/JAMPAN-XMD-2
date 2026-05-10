179a31a9917c04036375d8bc41a84b6e9a775633
Jampan-XDM <jampankelvin@gmail.com>
2026-05-10 22:24:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const cors = require('cors');
 const { startPairing } = require('./pair');
 
-const PORT = process.env.PORT || 7860;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
 app.use(express.static(path.join(__dirname, '')));
 
-// Route ya UI
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Endpoint ya kutoa kodi
 app.get('/code', async (req, res) => {
     const number = req.query.number;
-    if (!number) return res.status(400).json({ error: "Ingiza namba ya simu!" });
+    if (!number) return res.status(400).json({ error: "Weka namba!" });
 
     try {
         const pairingCode = await startPairing(number);
         res.json({ code: pairingCode });
     } catch (err) {
-        console.error("SERVER ERROR:", err.message);
-        res.status(500).json({ error: "WhatsApp Timeout! Subiri kidogo kisha jaribu tena." });
+        console.error("Pairing error:", err.message);
+        res.status(500).json({ error: "Imefeli kuunganisha. Jaribu tena." });
     }
 });
 
-app.listen(PORT, '0.0.0.0', () => {
+// Hii itazuia app isizimike hata kukiwa na unhandled error
+process.on('uncaughtException', (err) => {
+    console.error('There was an uncaught error', err);
+});
+
+app.listen(PORT, () => {
     console.log(`✅ JAMPAN XMD ONLINE ON PORT ${PORT}`);
 });
