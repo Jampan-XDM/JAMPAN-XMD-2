1adb18e158f004346daf9b9fe2e65cae46ce85b3
Jampan-XDM <jampankelvin@gmail.com>
2026-05-10 08:56:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    botName: "JAMPAN-XMD",
-    ownerName: "Kelvin Jampan",
-    channelLink: "https://whatsapp.com/channel/0029Vb7fTNf3QxS8A6rbBB3S",
-    channelJID: "120363409292513352@newsletter",
-    pairingVibe: "⚡ JAMPAN-XMD PAIRING SYSTEM"
-};
+    SESSION_PATH: "./sessions",
+    PREFIX: ".",
+    OWNER_NUMBER: "255674229015",
+    BOT_NAME: "JAMPAN XMD",
+    OWNER_NAME: "Kelvin Jampan"
+}
